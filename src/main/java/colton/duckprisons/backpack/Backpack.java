aafbd3102df9bc463889adbaece2984a5f730eec
TheCodingDuck <71715967+colton-boi@users.noreply.github.com>
2023-01-01 14:31:49 -0500

    Improved Backpack. Added init. Finished addItems()

--- src/main/java/colton/duckprisons/backpack/Backpack.java
+++ src/main/java/colton/duckprisons/backpack/Backpack.java
@@ -1,16 +1,33 @@
 package colton.duckprisons.backpack;
 
+import colton.duckprisons.PrisonPlayer;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
+import java.util.HashMap;
 
 public class Backpack {
-    public static void addItems(Player p, ItemStack items) {
 
+    public static void addItems(Player p, ItemStack items) {
+        PrisonPlayer.getBackpack(p).addItems(items);
     }
 
-    public static void addItems(Player p, List<ItemStack> items) {
-        items.forEach(itemStack -> addItems(p, itemStack));
+    private final PrisonPlayer player;
+    private final HashMap<Material, Long> storedItems;
+
+    public Backpack(PrisonPlayer p) {
+        this.player = p;
+        this.storedItems = new HashMap<>();
+    }
+
+    public void addItems(ItemStack items) {
+        Long amount = storedItems.getOrDefault(items.getType(), 0L);
+        amount+=items.getAmount();
+        storedItems.replace(items.getType(), amount);
+    }
+
+    public PrisonPlayer getPlayer() {
+        return player;
     }
 }
